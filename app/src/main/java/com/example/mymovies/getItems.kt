01ab7018a65756d23a1df4cf7e29762d2111589e
Winxester <aacuevasanta@gmail.com>
2021-10-12 16:23:10 +0200

    (refactor) getItems() restorded to older version. Reason: unresolver reference on module operation

--- app/src/main/java/com/example/mymovies/getItems.kt
+++ app/src/main/java/com/example/mymovies/getItems.kt
@@ -3,10 +3,15 @@ package com.example.mymovies
 import com.example.mymovies.MediaItem.*
 
 
-fun getItems(): List<MediaItem> = listOf(1..10).map {
-    MediaItem(
-        "Title $it",
-        "https://placekitten.com/200/200?image=$it",
-        if (it % 3 == 0) Type.VIDEO else Type.PHOTO
-    )
-}
+fun getItems(): List<MediaItem> = listOf(
+    MediaItem("title 1", "https://placekitten.com/200/200?image=1", Type.PHOTO),
+    MediaItem("title 2", "https://placekitten.com/200/200?image=2", Type.PHOTO),
+    MediaItem("title 3", "https://placekitten.com/200/200?image=3", Type.VIDEO),
+    MediaItem("title 4", "https://placekitten.com/200/200?image=4", Type.PHOTO),
+    MediaItem("title 5", "https://placekitten.com/200/200?image=5", Type.VIDEO),
+    MediaItem("title 6", "https://placekitten.com/200/200?image=6", Type.PHOTO),
+    MediaItem("title 7", "https://placekitten.com/200/200?image=7", Type.VIDEO),
+    MediaItem("title 8", "https://placekitten.com/200/200?image=8", Type.PHOTO),
+    MediaItem("title 9", "https://placekitten.com/200/200?image=9", Type.PHOTO),
+    MediaItem("title 10", "https://placekitten.com/200/200?image=10", Type.VIDEO)
+)
