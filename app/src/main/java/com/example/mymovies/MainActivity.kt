53b2dcd543cc3cf99a658727f54dc84e48d7ccd9
Winxester <aacuevasanta@gmail.com>
2021-10-12 16:19:28 +0200

    (menu) filter functionality added

--- app/src/main/java/com/example/mymovies/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.mymovies
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.Menu
+import android.view.MenuItem
 import com.example.mymovies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,9 +17,22 @@ class MainActivity : AppCompatActivity() {
 
         binding.recycler.adapter = adapter
 
-        adapter.mediaItems = getItems()
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        adapter.mediaItems = getItems().let { items ->
+            when (item.itemId) {
+                R.id.filter_all -> items
+                R.id.filter_photos -> items.filter { it.type == MediaItem.Type.PHOTO }
+                R.id.filter_videos -> items.filter { it.type == MediaItem.Type.VIDEO }
+                else -> emptyList()
+            }
+        }
+        return true
     }
 }
