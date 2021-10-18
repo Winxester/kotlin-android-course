36c0d96022c319062be9321ce6f4dc5360197706
Winxester <aacuevasanta@gmail.com>
2021-10-18 22:43:04 +0200

    feat: provide data to DetailActivity.kt and update UI

--- app/src/main/java/com/example/mymovies/DetailActivity.kt
+++ app/src/main/java/com/example/mymovies/DetailActivity.kt
@@ -1,8 +1,15 @@
 package com.example.mymovies
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import com.example.mymovies.MediaItem.*
 import com.example.mymovies.databinding.ActivityDetailBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class DetailActivity : AppCompatActivity() {
 
@@ -15,9 +22,20 @@ class DetailActivity : AppCompatActivity() {
         val binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //intent.getIntExtra(EXTRA_ID,)
+        val itemId = intent.getIntExtra(EXTRA_ID, -1)
 
-        // Ver vídeo para realizar ejercicio
+        lifecycleScope.launch {
+            val items = withContext(Dispatchers.IO) { MediaProvider.getItems() }
+            val item : MediaItem? = items.firstOrNull { it.id == itemId}
 
+            item?.let {
+                supportActionBar?.title = item.title
+                binding.detailThumb.loadUrl(item.url)
+                binding.detailVideoIndicator.visibility = when (item.type) {
+                    Type.VIDEO -> View.VISIBLE
+                    Type.PHOTO -> View.GONE
+                }
+            }
+        }
     }
 }
